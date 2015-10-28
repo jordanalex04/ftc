d17e78873477ddf710cde03658ee1edf64a796e8
Maxwell Dausch <max.dausch@gmail.com>
2015-10-28 15:11:10 -0500

    Created Test Autonomous mode v.1

--- ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TestAlexander.java
+++ ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TestAlexander.java
@@ -1,11 +1,13 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 /**
  * Created by FentonVideo on 10/27/2015.
  */
 public class TestAlexander extends OpMode {
+
 
     @Override
     public void init() {
